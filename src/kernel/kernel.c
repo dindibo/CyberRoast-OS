2f507d5932e5d5f6b6fd132d7ef6294db0ebbe98
Your Name <you@example.com>
2023-04-08 22:46:35 +0000

    Added Title

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,7 +1,37 @@
+extern void putc ();
+
+#define MMIO_TEXT_PRINT (0xB8000)
+
+const char arrTest[] = "[ Welcome to Nivux-1 ]";
+const char clearText[] = "                                                                                ";
+
+void testText () {
+	*((int*)0xb8000)=0x07690748;
+	*((int*)0xb8004)=0x07690748;
+}
+
+void write_string( int colour, const char *string )
+{
+    volatile char *video = (volatile char*)0xB8000;
+    while( *string != 0 )
+    {
+        *video++ = *string++;
+        *video++ = colour;
+    }
+}
+
+void clearScreen(){
+	write_string(0, clearText);
+}
+
 int kmain(void *mbd,unsigned int magic){
 	if (magic!=0x2BADB002){
 		return -1;
 	}
+
+	clearScreen();
+	write_string(3, arrTest);
+
 	return 0;
 }
 
